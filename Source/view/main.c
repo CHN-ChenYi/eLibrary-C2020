54fb2e21c0dd34c160e6c73b121179e2221bd2bc
TO/GA <chenyi2000@zju.edu.cn>
2020-04-21 22:49:49 +0800

    init

---
+++ Source/view/main.c
@@ -0,0 +1,15 @@
+#include "exception.h"
+#include "graphics.h"
+
+void Main() {
+  InitGraphics();
+  InitConsole();
+  const double cx = GetWindowWidth() / 2;
+  const double cy = GetWindowHeight() / 2;
+  LibImage img;
+  try {
+    loadImage("./Resource/test.jpg", &img);
+    except(ErrorException) puts("oops");
+  }
+  endtry DrawImage(&img, cx - 1, cy - 1.5, 2, 3);
+}

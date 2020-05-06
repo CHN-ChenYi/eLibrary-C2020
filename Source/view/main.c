d9150d728e439ff275e7be90e6a6c7c5f8f95e11
TO/GA <chenyi2000@zju.edu.cn>
2020-05-06 15:20:38 +0800

    SelectFile can select a file that doesn't exist now

--- Source/view/main.c
+++ Source/view/main.c
@@ -1,4 +1,5 @@
 #include "exception.h"
+#include "genlib.h"
 #include "graphics.h"
 
 void Draw(LibImage img, const double cx, const double cy) {
@@ -12,11 +13,11 @@ void Main() {
   LibImage img;
   try {
     char path[200];
-    SelectFile("JPG image (*.jpg|*.jpeg|*.jpe)\0*.jpg;*.jpeg;*.jpe\0", path,
-               200 - 1);
+    SelectFile("JPG image (*.jpg|*.jpeg|*.jpe)\0*.jpg;*.jpeg;*.jpe\0", "jpg",
+               FALSE, path, 200 - 1);
     puts(path);
     loadImage(path, &img);
     except(ErrorException) puts("oops");
-  } endtry
+  } endtry;
   Draw(img, GetWindowWidth() / 2, GetWindowHeight() / 2);
 }

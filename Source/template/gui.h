4ce6e2c7fcb9f5d5d417813cafcfa0b2e903f8fb
Hans <2655098959@qq.com>
2020-05-06 11:47:35 +0800

    modify gui.h

--- Source/template/gui.h
+++ Source/template/gui.h
@@ -42,8 +42,7 @@ struct LendAndBorrow {
   Book *book;                                 // 书籍信息
   List *book_list;                            // 待借/还书列表
   void (*search_callback) (char* keyword);    // 搜索按钮
-  void (*button_callback) ();                 // 确定/放弃按钮
-  void (*return_callback) ();                 // 返回按钮
+  void (*return_callback) (ListNode* book);   // 还书按钮
 };
 
 /* 搜索界面 */
@@ -51,7 +50,7 @@ struct Search {
   char *keyword;                              // 搜索关键词
   List *search_result;                        // 结果链表
   void (*search_callback) (char* keyword);    // 搜索按钮
-  void (*return_callback) ();                 // 返回按钮
+  void (*borrow_callback) (Book* book);       // 借书按钮
 };
 
 /* 用户手册/关于界面 */
@@ -59,28 +58,25 @@ struct Manual {
   enum {kManual, kAbout} Type;    // 用户手册/关于
   char *title;
   char *content;
-  void (*return_callback) ();     // 返回按钮
 };
 
 /* 用户登陆/注册界面 */
 struct UserLogIn {
   enum {kLogin, kRegister} Type;  // 登录还是注册
-  User *user;                      // 当前正在登陆/注册的这个用户
+  User *user;                     // 当前正在登陆/注册的这个用户
   char password[50];              // 注册/登陆的密码
   char repeat_password[50];       // 重复新密码
   void (*login_callback) ();      // 登陆/注册按钮
-  void (*return_callback) ();     // 返回按钮
 };
 
 /* 用户信息修改 */
 struct UserModify {
-  User *user;                    // 用户信息
+  User *user;                   // 用户信息
   char old_password[50];        // 旧密码
   char new_password[50];        // 新密码
   char repeat_password[50];     // 重复新密码
   void (*confirm_callback) ();  // 确认按钮
   void (*admin_callback) ();    // 管理员修改他人账户按钮
-  void (*return_callback) ();   // 返回按钮
 };
 
 
@@ -89,36 +85,31 @@ struct UserManagement {
   List *to_be_verified;               // 待审核用户列表
   List *users;                        // 已添加用户列表
   void (*callback) (ListNode* user);  // 审核通过或者拒绝
-  void (*return_callback) ();         // 返回按钮
 };
 
 /* 图书库界面 */
 struct Library {
-  enum {kPicture, kList} Type;               // 图片模式还是列表模式
+  enum {kPicture, kList} Type;              // 图片模式还是列表模式
   List *books;                              // 图书库的图书
   void (*sort_callback) ();                 // 排序按钮
   void (*book_callback) (ListNode* book);   // 图书详细信息按钮
-  void (*return_callback) ();               // 返回按钮
 };
 
 /* 图书显示、新建、修改 */
 struct BookDisplay {
   enum {kDisplay, kInit, kModify} Type;   // 当前状态：显示/新建/删除
-  Book *book;                              // 当前书籍
+  Book *book;                             // 当前书籍
   void (*admin_callback) ();              // 查看图书借阅次数按钮（管理员）
-  void (*return_callback) ();             // 返回按钮
+  void (*borrow_callback) (Book* book);   // 借书按钮
 };
 
 /* 图书借还界面显示 */
 struct BorrowDisplay {
   char *book_name;                    // 当前书籍
   List *borrow_record;                // 当前书籍的借还记录
-  void (*confirm_callback) ();        // 确认按钮
-  void (*return_callback) ();         // 返回按钮
 };
 
 /* 统计界面 */
 struct Statistics {
   List *borrow_record;                // 借还次数统计
-  void (*return_callback) ();         // 返回按钮
 };

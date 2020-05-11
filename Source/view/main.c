6b8aae7687034a0056b39905e5852b52accef43e
TO/GA <chenyi2000@zju.edu.cn>
2020-05-11 08:45:35 +0800

    Fix

--- Source/view/main.c
+++ Source/view/main.c
@@ -3,20 +3,34 @@
 #include "graphics.h"
 #include "ui.h"
 
+Button *CreateButton(Rect rect, char *caption) {
+  Button *ret = malloc(sizeof(Button));
+  ret->position = rect;
+  strcpy(ret->caption, caption);
+  return ret;
+}
+
+InputBox *CreateInputBox(Rect rect, char *context) {
+  InputBox *ret = malloc(sizeof(InputBox));
+  ret->position = rect;
+  strcpy(ret->context, context);
+  return ret;
+}
+
 void Main() {
   InitGraphics();
   InitConsole();
 
-  Button button1 = (Button){(Rect){100, 500, 100, 200}, "Fuck you!"};
-  Button button2 = (Button){ (Rect) { 100, 500, 300, 400 }, "Fuck you!" };
-  Button button3 = (Button){ (Rect) { 100, 500, 500, 600 }, "Fuck you!" };
+  Button *button1 = CreateButton((Rect){100, 500, 100, 200}, "Fuck you!");
+  Button *button2 = CreateButton((Rect) { 100, 500, 300, 400 }, "Fuck you!");
+  Button *button3 = CreateButton((Rect){100, 500, 500, 600}, "Fuck you!");
 
-  InputBox input_box = (InputBox){(Rect){100, 500, 100, 500}, "Fuck you!"};
+  InputBox *input_box = CreateInputBox((Rect){100, 500, 100, 500}, "Fuck you!");
   InitComponents();
-  InsertComp(&button1, kButton);
-  InsertComp(&button2, kButton);
-  InsertComp(&button3, kButton);
-  InsertComp(&input_box, kInput);
+  InsertComp(button1, kButton);
+  InsertComp(button2, kButton);
+  InsertComp(button3, kButton);
+  InsertComp(input_box, kInput);
   DrawComponents();
   registerMouseEvent(MouseMoveEventHandler);
   //MovePen(100, 100);

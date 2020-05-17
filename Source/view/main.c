48e4c8491aceabe7e7ef19faf2653d2908224365
Hans <2655098959@qq.com>
2020-05-17 18:30:44 +0800

    fix Chinese input

--- Source/view/main.c
+++ Source/view/main.c
@@ -4,8 +4,17 @@
 #include "extgraph.h"
 #include "page.h"
 #include "gui.h"
+#include "ui.h"
 
 
 void Main() {
-  InitGUI();
+  //InitGUI();
+  InitGraphics();
+  InitConsole();
+  InitializeUI();
+  InputBox *input_box = CreateInputBox(
+    (Rect){100, 200, 100, 200}, "", 0
+  );
+  InsertComp(input_box, kInputBox);
+  
 }
